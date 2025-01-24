6fcab1a9d9fbb3190b3abb7e371db987c49ee213
Rajkumar Nagarasa <rnagarasa@myseneca.ca>
2025-01-24 15:05:16 -0500

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+Author: Rajkumar Nagarasa
+Author ID: 071788145
+Date Created: 2025/01/24
+'''
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
