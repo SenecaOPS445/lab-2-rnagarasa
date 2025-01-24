7aa1804985d441e77922a62aa794d7112e4e5128
Rajkumar Nagarasa <rnagarasa@myseneca.ca>
2025-01-24 15:58:26 -0500

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+Author: Rajkumar Nagarasa
+Author ID: 071788145
+Date Created: 2025/01/24
+'''
+
+import sys
+
+timer = int(sys.argv[1])
+
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+
+print('blast off!')
